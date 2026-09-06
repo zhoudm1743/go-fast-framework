8ee19996879cec7a887a09d58c117bf800087f74
zhoudm1743 <zhoudm1743@163.com>
2026-09-06 10:01:41 +0800

    chore: 发布 0.8.2（Response 响应哨兵根治双写缺陷）

--- foundation/application.go
+++ foundation/application.go
@@ -9,7 +9,7 @@ import (
 	"github.com/zhoudm1743/go-fast-framework/contracts"
 )
 
-const Version = "0.8.1"
+const Version = "0.8.2"
 
 // Application 应用实例接口，嵌入 Container。
 type Application interface {
